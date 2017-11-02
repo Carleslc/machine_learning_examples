c65ad70eeaec3813b9b3e45243c53d905160cfb5
Carleslc <lazaro.costa.carles@gmail.com>
2017-11-02 23:05:43 +0100

    NumPy matrices

--- numpy_class/examples.py
+++ numpy_class/examples.py
@@ -17,7 +17,7 @@ def dot_product(a, b):
 print("Python Lists")
 a = [1, 2, 3]
 b = [4, 5, 6]
-print(f"a: {a}\nb: {b}")
+print(f"a: { a }\nb: { b }")
 print(f"a + b: { a + b }")
 print(f"elements a + b: { [e1 + e2 for e1, e2 in zip(a, b)] }")
 print(f"2 * a: { 2 * a }")
@@ -35,7 +35,7 @@ print()
 print("NumPy Arrays")
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
-print(f"a: {a}\nb: {b}")
+print(f"a: { a }\nb: { b }")
 print(f"a + b: { a + b }")
 print(f"2 * a: { 2 * a }")
 print(f"a * b: { a * b }")
@@ -49,3 +49,15 @@ print(f"sqrt a: { np.sqrt(a) }")
 print(f"a^b: { a**b }")
 print(f"e^a: { np.exp(a) }")
 print(f"log a: { np.log(a) }")
+
+print()
+
+print("NumPy Matrices")
+m = np.array([[1, 2], [3, 4]])
+print(f"m as array of arrays:\n{ m }\n{ type(m) }")
+print(f"element at (0,0): { m[0,0] }") # Equivalent: m[0][0]
+m = np.matrix([[1, 2], [3, 4]])
+print(f"m as matrix:\n{ m }\n{ type(m) }")
+print(f"element at (0,0): { m[0,0] }") # Equivalent: m[0][0]
+print(f"convert matrix to array:\n{ np.array(m) }")
+print(f"transpose matrix:\n{ m.T }")
